74f2a8933d147faeb4587d63e06e305275663123
EC2 Default User <ec2-user@ip-172-31-77-222.ec2.internal>
2026-04-04 12:21:17 +0000

    eb fix

--- fog_nodes/fog_node.py
+++ fog_nodes/fog_node.py
@@ -2,7 +2,7 @@ import requests
 import time
 from sensors.sensor_sim import generate_sensor_data
 
-CLOUD_ENDPOINT = "http://fog-env.eba-9uvt5trc.us-east-1.elasticbeanstalk.com/upload"
+CLOUD_ENDPOINT = "http://fog-env.eba-9uvt5trc.us-east-1.elasticbeanstalk.com/"
 
 def fog_node_process(sensor_data):
     processed = {}
